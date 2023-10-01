366c713db7ab08374d4d63717a4008f2ccc25534
ALI-BABAI <aliullov22@mail.ru>
2023-10-01 15:41:16 +0400

    Набросок Onboarding screen
    Переход на другой экран после 2секундной задержки
    При добавление изображения размер столбца == размерам изображения.
    Убрав Scaffold столбец занимает всю область экрана.
    Обернув Image в виджет Expanded проблема не решается

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:weather_forecast_app/theme/text.dart';
 import 'theme/colors.dart';
+import 'screens/first_openning_screen.dart';
 
 void main() {
-  runApp(const InitializationScreen());
+  runApp(const MaterialApp(
+    title: 'Weather forecast',
+    home: InitializationScreen(),
+  ));
 }
 
 class InitializationScreen extends StatelessWidget {
@@ -12,47 +16,47 @@ class InitializationScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Weather forecast',
-        // theme: ThemeData.dark(),
-        home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              gradient: LinearGradient(
-                colors: [Color(0xFFBCC8D6), Color(0xFFF2F4F7)],
-                begin: Alignment.bottomCenter,
-                end: Alignment.center,
-                // tileMode: TileMode.repeated,
-              ),
-            ),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Image(
-                      image: AssetImage('assets/images/mainScreen.png')),
-                  const SizedBox(height: 5),
-                  Text(
-                    'Weather',
-                    style: poppinsRegularExtended(
-                        40, darkBlueColor, FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Forecast',
-                    style:
-                        poppinsRegularExtended(33, grayColor, FontWeight.w400),
-                  ),
-                  const SizedBox(
-                    height: 150,
-                  ),
-                ],
-              ),
-            ),
+    // Задержка в 2-3 секунды перед переходом
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const FirstOpenningScreens()),
+      );
+    });
+
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          gradient: LinearGradient(
+            colors: [Color(0xFFBCC8D6), Color(0xFFF2F4F7)],
+            begin: Alignment.bottomCenter,
+            end: Alignment.center,
+            // tileMode: TileMode.repeated,
           ),
-        ));
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(image: AssetImage('assets/images/mainScreen.png')),
+              const SizedBox(height: 5),
+              Text(
+                'Weather',
+                style:
+                    poppinsRegularExtended(40, darkBlueColor, FontWeight.w600),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                'Forecast',
+                style: poppinsRegularExtended(33, grayColor, FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/first_openning_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:weather_forecast_app/theme/text.dart';
+import 'package:weather_forecast_app/theme/colors.dart';
+
+void btnScipPressed() {}
+
+class FirstOpenningScreens extends StatelessWidget {
+  const FirstOpenningScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color(0xFF484B5B), Color(0xFF2C2D35)],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              tileMode: TileMode.decal),
+        ),
+        child: Column(children: [
+          const SizedBox(height: 200),
+          TextButton(
+            onPressed: btnScipPressed,
+            child: Text(
+              'skip',
+              style: poppinsRegularExtended(14, whiteColor, FontWeight.w500),
+            ),
+          ),
+          const Expanded(
+            child: Image(
+                image: AssetImage('assets/images/firsOpennigScreen1.png')),
+          ),
+          const SizedBox(height: 50),
+          Text(
+            'Real-Time',
+            style: poppinsRegularExtended(28, darkBlueColor, FontWeight.w600),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            'Weather Map',
+            style: poppinsRegularExtended(28, darkBlueColor, FontWeight.w600),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            'Watch the progress of the',
+            style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            'precipitation to stay informed',
+            style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+          ),
+          const SizedBox(height: 100),
+          Text(
+            'BTN',
+            style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class FirstOpenningScreenNext extends StatelessWidget {
+  const FirstOpenningScreenNext({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF484B5B), Color(0xFF2C2D35)],
+          begin: Alignment.bottomCenter,
+          end: Alignment.center,
+        ),
+      ),
+      child: Column(children: [
+        TextButton(
+          onPressed: btnScipPressed,
+          child: Text(
+            'skip',
+            style: poppinsRegularExtended(14, whiteColor, FontWeight.w500),
+          ),
+        ),
+        const Image(
+            image: AssetImage('assets/images/firsOpennigScreen1.png'),
+            height: 250,
+            width: 250),
+        const SizedBox(height: 200),
+        Text(
+          'Real-Time',
+          style: poppinsRegularExtended(28, darkBlueColor, FontWeight.w600),
+        ),
+        Text(
+          'лол кек',
+          style: poppinsRegularExtended(28, darkBlueColor, FontWeight.w600),
+        ),
+        Text(
+          'терентий',
+          style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+        ),
+        Text(
+          'акакьевич',
+          style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+        ),
+        const SizedBox(height: 200),
+        Text(
+          'BTN',
+          style: poppinsRegularExtended(16, grayColor, FontWeight.w400),
+        ),
+      ]),
+    );
+  }
+}
