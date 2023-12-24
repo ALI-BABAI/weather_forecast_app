f99a1b476a554278ae1c66cd46c3cb1ced749c86
ALI-BABAI <aliullov22@mail.ru>
2023-12-24 18:26:22 +0400

    Удаление неиспользуемой папки(первые шаги в работе с запросами)

--- lib/screens/settings_screen/widgets/tools_widget.dart
+++ lib/screens/settings_screen/widgets/tools_widget.dart
@@ -21,8 +21,6 @@ final List settingsItems = [
     function: () {
       debugPrint('handling...');
     },
-    //settings_system_daydream_outlined
-    //balance_rounded
   ),
   SettingButtonItem(
     buttonName: 'Notifications',
@@ -104,92 +102,93 @@ class ToolsElementWidget extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: TextButton(
-            onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    backgroundColor: AppColors.appBackground,
-                    title: const Text(
-                      'App unit\'s:',
-                      style: AppTextStyles.settingsScreenHeaderFont,
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              backgroundColor: AppColors.appBackground,
+              title: const Text(
+                'App unit\'s:',
+                style: AppTextStyles.settingsScreenHeaderFont,
+              ),
+              actionsAlignment: MainAxisAlignment.spaceAround,
+              actions: <Widget>[
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        'Temperature:',
+                        style: AppTextStyles.expandedMainFont,
+                      ),
                     ),
-                    actionsAlignment: MainAxisAlignment.spaceAround,
-                    actions: <Widget>[
-                      Row(
-                        children: [
-                          const Expanded(
-                            child: Text(
-                              'Temperature:',
-                              style: AppTextStyles.expandedMainFont,
-                            ),
-                          ),
-                          TextButton(
-                            style: const ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll(AppColors.orange)),
-                            onPressed: () => Navigator.pop(context, '°C'),
-                            child: const Text(
-                              '°C',
-                              style: AppTextStyles.expandedMainFont,
-                            ),
-                          ),
-                          const SizedBox(width: 15),
-                          TextButton(
-                            style: const ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll(AppColors.orange)),
-                            onPressed: () => Navigator.pop(context, '°F'),
-                            child: const Text(
-                              '°F',
-                              style: AppTextStyles.expandedMainFont,
-                            ),
-                          ),
-                        ],
+                    TextButton(
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(AppColors.orange)),
+                      onPressed: () => Navigator.pop(context, '°C'),
+                      child: const Text(
+                        '°C',
+                        style: AppTextStyles.expandedMainFont,
                       ),
-                      Row(
-                        children: [
-                          const Expanded(
-                              child: Text(
-                            'Pressure:',
-                            style: AppTextStyles.expandedMainFont,
-                          )),
-                          TextButton(
-                            style: const ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll(AppColors.orange)),
-                            onPressed: () => Navigator.pop(context, '°C'),
-                            child: const Text(
-                              'hPa',
-                              style: AppTextStyles.expandedMainFont,
-                            ),
-                          ),
-                          const SizedBox(width: 15),
-                          TextButton(
-                            style: const ButtonStyle(
-                                backgroundColor:
-                                    MaterialStatePropertyAll(AppColors.orange)),
-                            onPressed: () => Navigator.pop(context, '°F'),
-                            child: const Text(
-                              'mmHg',
-                              style: AppTextStyles.expandedMainFont,
-                            ),
-                          ),
-                        ],
+                    ),
+                    const SizedBox(width: 15),
+                    TextButton(
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(AppColors.orange)),
+                      onPressed: () => Navigator.pop(context, '°F'),
+                      child: const Text(
+                        '°F',
+                        style: AppTextStyles.expandedMainFont,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-            child: Row(
-              children: [
-                Icon(setting.iconType, color: AppColors.white, size: 30),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Text(
-                    setting.buttonName,
-                    style: AppTextStyles.expandedMainFont,
-                  ),
+                Row(
+                  children: [
+                    const Expanded(
+                        child: Text(
+                      'Pressure:',
+                      style: AppTextStyles.expandedMainFont,
+                    )),
+                    TextButton(
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(AppColors.orange)),
+                      onPressed: () => Navigator.pop(context, '°C'),
+                      child: const Text(
+                        'hPa',
+                        style: AppTextStyles.expandedMainFont,
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    TextButton(
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(AppColors.orange)),
+                      onPressed: () => Navigator.pop(context, '°F'),
+                      child: const Text(
+                        'mmHg',
+                        style: AppTextStyles.expandedMainFont,
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            )),
+            ),
+          ),
+          child: Row(
+            children: [
+              Icon(setting.iconType, color: AppColors.white, size: 30),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  setting.buttonName,
+                  style: AppTextStyles.expandedMainFont,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
