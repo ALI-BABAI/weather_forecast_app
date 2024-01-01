c3109103f2a1174db8676dbda66186a3b3c9efbb
ALI-BABAI <aliullov22@mail.ru>
2024-01-01 12:32:47 +0400

    Закомментирован неиспользуемый файл экрана первого открытия приложения

--- lib/screens/welcome_screen/first_openning_screen.dart
+++ lib/screens/welcome_screen/first_openning_screen.dart
@@ -1,120 +1,120 @@
-import 'package:flutter/material.dart';
-import 'package:weather_forecast_app/theme/app_text_styles.dart';
-import 'package:weather_forecast_app/theme/app_colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:weather_forecast_app/theme/app_text_styles.dart';
+// import 'package:weather_forecast_app/theme/app_colors.dart';
 
-void btnScipPressed() {}
+// void btnScipPressed() {}
 
-class FirstOpenningScreens extends StatelessWidget {
-  const FirstOpenningScreens({super.key});
+// class FirstOpenningScreens extends StatelessWidget {
+//   const FirstOpenningScreens({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: AppColors.gradientBackground,
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              tileMode: TileMode.decal),
-        ),
-        child: Column(children: [
-          const SizedBox(height: 200),
-          TextButton(
-            onPressed: btnScipPressed,
-            child: Text(
-              'skip',
-              style:
-                  poppinsRegularExtended(14, AppColors.white, FontWeight.w500),
-            ),
-          ),
-          const Expanded(
-            child: Image(
-                image: AssetImage('assets/images/firsOpennigScreen1.png')),
-          ),
-          const SizedBox(height: 50),
-          Text(
-            'Real-Time',
-            style:
-                poppinsRegularExtended(28, AppColors.darkBlue, FontWeight.w600),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            'Weather Map',
-            style:
-                poppinsRegularExtended(28, AppColors.darkBlue, FontWeight.w600),
-          ),
-          const SizedBox(height: 15),
-          Text(
-            'Watch the progress of the',
-            style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            'precipitation to stay informed',
-            style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-          ),
-          const SizedBox(height: 100),
-          Text(
-            'BTN',
-            style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-          ),
-        ]),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//               colors: AppColors.gradientBackground,
+//               begin: Alignment.bottomCenter,
+//               end: Alignment.topCenter,
+//               tileMode: TileMode.decal),
+//         ),
+//         child: Column(children: [
+//           const SizedBox(height: 200),
+//           TextButton(
+//             onPressed: btnScipPressed,
+//             child: Text(
+//               'skip',
+//               style:
+//                   poppinsFont(14, AppColors.white, FontWeight.w500),
+//             ),
+//           ),
+//           const Expanded(
+//             child: Image(
+//                 image: AssetImage('assets/images/firsOpennigScreen1.png')),
+//           ),
+//           const SizedBox(height: 50),
+//           Text(
+//             'Real-Time',
+//             style:
+//                 poppinsFont(28, AppColors.darkBlue, FontWeight.w600),
+//           ),
+//           const SizedBox(height: 5),
+//           Text(
+//             'Weather Map',
+//             style:
+//                 poppinsFont(28, AppColors.darkBlue, FontWeight.w600),
+//           ),
+//           const SizedBox(height: 15),
+//           Text(
+//             'Watch the progress of the',
+//             style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//           ),
+//           const SizedBox(height: 5),
+//           Text(
+//             'precipitation to stay informed',
+//             style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//           ),
+//           const SizedBox(height: 100),
+//           Text(
+//             'BTN',
+//             style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
 
-class FirstOpenningScreenNext extends StatelessWidget {
-  const FirstOpenningScreenNext({super.key});
+// class FirstOpenningScreenNext extends StatelessWidget {
+//   const FirstOpenningScreenNext({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: AppColors.gradientBackground,
-          begin: Alignment.bottomCenter,
-          end: Alignment.center,
-        ),
-      ),
-      child: Column(children: [
-        TextButton(
-          onPressed: btnScipPressed,
-          child: Text(
-            'skip',
-            style: poppinsRegularExtended(14, AppColors.white, FontWeight.w500),
-          ),
-        ),
-        const Image(
-            image: AssetImage('assets/images/firsOpennigScreen1.png'),
-            height: 250,
-            width: 250),
-        const SizedBox(height: 200),
-        Text(
-          'Real-Time',
-          style:
-              poppinsRegularExtended(28, AppColors.darkBlue, FontWeight.w600),
-        ),
-        Text(
-          'лол кек',
-          style:
-              poppinsRegularExtended(28, AppColors.darkBlue, FontWeight.w600),
-        ),
-        Text(
-          'терентий',
-          style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-        ),
-        Text(
-          'акакьевич',
-          style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-        ),
-        const SizedBox(height: 200),
-        Text(
-          'BTN',
-          style: poppinsRegularExtended(16, AppColors.gray, FontWeight.w400),
-        ),
-      ]),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: AppColors.gradientBackground,
+//           begin: Alignment.bottomCenter,
+//           end: Alignment.center,
+//         ),
+//       ),
+//       child: Column(children: [
+//         TextButton(
+//           onPressed: btnScipPressed,
+//           child: Text(
+//             'skip',
+//             style: poppinsFont(14, AppColors.white, FontWeight.w500),
+//           ),
+//         ),
+//         const Image(
+//             image: AssetImage('assets/images/firsOpennigScreen1.png'),
+//             height: 250,
+//             width: 250),
+//         const SizedBox(height: 200),
+//         Text(
+//           'Real-Time',
+//           style:
+//               poppinsFont(28, AppColors.darkBlue, FontWeight.w600),
+//         ),
+//         Text(
+//           'лол кек',
+//           style:
+//               poppinsFont(28, AppColors.darkBlue, FontWeight.w600),
+//         ),
+//         Text(
+//           'терентий',
+//           style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//         ),
+//         Text(
+//           'акакьевич',
+//           style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//         ),
+//         const SizedBox(height: 200),
+//         Text(
+//           'BTN',
+//           style: poppinsFont(16, AppColors.gray, FontWeight.w400),
+//         ),
+//       ]),
+//     );
+//   }
+// }
