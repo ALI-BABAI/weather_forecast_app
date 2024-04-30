b0de705ad982c222c896e8def68908c1d7e06f39
ALI-BABAI <aliullov22@mail.ru>
2024-04-30 16:10:25 +0400

    mock login

--- lib/presenter/screens/login_screen/login_screen.dart
+++ lib/presenter/screens/login_screen/login_screen.dart
@@ -1,100 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_forecast_app/domain/bloc/login_bloc/bloc/login_bloc.dart';
+import 'package:weather_forecast_app/domain/repository/login_repository.dart';
 import 'package:weather_forecast_app/generated/l10n.dart';
 
+import '../../theme/app_decoration.dart';
 import '../loading_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen(this.loginRepository, {super.key});
+
+  final LoginRepository loginRepository;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<LoginBloc, LoginState>(
-      listener: (context, state) {
-        if (state is LoginFailureState) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(S.of(context).authFailureMessage),
-              backgroundColor: Colors.red.shade400,
-              duration: const Duration(seconds: 30),
-              showCloseIcon: true,
-            ),
-          );
-        }
-      },
-      builder: (context, state) {
-        if (state is LoadingState) {
-          return const LoadingScreen(widget: CircularProgressIndicator());
-        } else {
-          return LoadingScreen(
-            widget: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: TextField(
-                    controller: _emailController,
-                    maxLength: 32,
-                    style: const TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                      labelText: S.of(context).email,
-                      labelStyle: const TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: TextField(
-                    controller: _passwordController,
-                    maxLength: 32,
-                    obscureText: true,
-                    style: const TextStyle(color: Colors.amber),
-                    decoration: InputDecoration(
-                        labelText: S.of(context).password,
-                        labelStyle: const TextStyle(color: Colors.black),
-                        fillColor: Colors.amber),
-                  ),
-                ),
-                const SizedBox(height: 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(
-                      width: 150,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.amber),
-                        ),
-                        child: Text(
-                          S.of(context).createAccount,
-                          textAlign: TextAlign.center,
-                        ),
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: AppDecorations.lightDecorationTheme,
+        child: ListView(
+          children: [
+            BlocProvider<LoginBloc>(
+              create: (context) => LoginBloc(loginRepository),
+              child: BlocConsumer<LoginBloc, LoginState>(
+                listener: (context, state) {
+                  if (state is LoginSuccessState) {
+                    Navigator.pushReplacementNamed(context, '/weather');
+                  }
+                  if (state is LoginFailureState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(S.of(context).authFailureMessage),
+                        backgroundColor: Colors.red.shade400,
+                        duration: const Duration(seconds: 5),
+                        showCloseIcon: true,
                       ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/weather');
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.amber),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return LoadingScreen(
+                    widget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 24),
+                          child: TextField(
+                            controller: _emailController,
+                            maxLength: 32,
+                            style: const TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                              labelText: S.of(context).email,
+                              labelStyle: const TextStyle(color: Colors.black),
+                            ),
+                          ),
                         ),
-                        child: Text(S.of(context).login),
-                      ),
+                        const SizedBox(height: 16),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 24),
+                          child: TextField(
+                            controller: _passwordController,
+                            maxLength: 32,
+                            obscureText: true,
+                            style: const TextStyle(color: Colors.amber),
+                            decoration: InputDecoration(
+                                labelText: S.of(context).password,
+                                labelStyle:
+                                    const TextStyle(color: Colors.black),
+                                fillColor: Colors.amber),
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.amber),
+                                ),
+                                child: Text(
+                                  S.of(context).createAccount,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 150,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  context.read<LoginBloc>().add(
+                                        CheckLoginEvent(
+                                          email: _emailController.text,
+                                          password: _passwordController.text,
+                                        ),
+                                      );
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.amber),
+                                ),
+                                child: Text(S.of(context).login),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  );
+                },
+              ),
             ),
-          );
-        }
-      },
+          ],
+        ),
+      ),
     );
   }
 }
